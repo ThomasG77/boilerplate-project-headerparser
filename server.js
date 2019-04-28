91bb694733d4ac526a739c0fea0de8d7045e85db
Thomas Gratier <thomas_gratier@yahoo.fr>
2019-04-29 00:12:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+app.set('trust proxy', true);
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -24,9 +25,16 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami", function (req, res) {  
+  res.json({
+    "ipaddress": req.ip,
+    "language": req.header('Accept-Language'),
+    "software": req.header('User-Agent')
+  });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
